10cc250c886aa0ba53a800129616d6a32db24a4e
Stephanie Yomorta <stephanie.yomorta@gmail.com>
2017-01-17 16:21:53 -0800

    So far so good...

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -8,8 +8,14 @@ class PatientsController < ApplicationController
   end
 
   def create
-    current_user.patients.create(params.require(:patient).permit(:first_name, :last_name, :age, :gender, :home_session, :school_session))
-    redirect_to patients_path
+    @patient = Patient.new(patient_params)
+    # @patient.observation = @observation
+    # @patient.user = current_user
+    if @patient.save
+      redirect_to @patient
+    else
+      render :new
+    end
   end
 
   def show
@@ -26,10 +32,26 @@ class PatientsController < ApplicationController
     redirect_to @patient
   end
 
-  # def destroy
-  #   @patient = Patient.find(params[:id])
-  #   current_user.observations.where(patient: @patient).destroy_all
-  #   redirect_to patients_path
-  # end
+  def destroy
+    @patient = Patient.find(params[:id])
+    current_user.observations.where(patient_id: @patient).destroy_all
+    @patient.destroy if @patient.observations.count == 0
+    redirect_to patients_path
+    # p params
+    # @observation = @patient.observations
+    # if @patient.observations.count == 1 && (@observation.user_id == current_user.id)
+    #   p '*' * 50
+    #   p @patient
+    #   # @patient.destroy
+    #   Patient.destroy(@patient)
+    #   redirect_to patients_path
+    # else
+    # end
+  end
+
+  private
+  def patient_params
+    params.require(:patient).permit(:first_name, :last_name, :age, :gender, :home_session, :school_session)
+  end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,6 @@ class User < ActiveRecord::Base
   has_attached_file :image, styles: { medium: "500x500>", thumb: "250x250>" }, presence: true
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
-  has_many :observations
+  has_many :observations, dependent: :destroy
   has_many :patients, through: :observations
 end
